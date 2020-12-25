7641160cc53793842d95aebdd350c3ba36192c8b
Charles Wang <wcharles168gmail.com>
2020-12-25 15:46:21 -0500

    added monorepo; begin google oauth

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {LandingComponent} from './components/landing.js'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <LandingComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/landing.js
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from 'react'
+
+export function LandingComponent() {
+    // const [auth2, setAuth] = useState(null)
+    // useEffect(() => {
+    //     gapi.load('auth2', () => {
+    //         setAuth(gapi.auth2.init({
+    //             client_id: '681660428307-tlu6t58okkk8i6vi2fe4dm39i933f548.apps.googleusercontent.com'
+    //         }))
+    //     })
+    // })
+    // function onSignIn() {
+    //     gapi.load('auth2', () => {
+
+    //     })
+    // }
+    async function login() {
+        const response = await fetch('http://localhost:8000/auth/google')
+    }
+    return (
+        <React.Fragment>
+            <button onClick={login}>Login</button>
+            {/* <div class="g-signin2" data-onsuccess="onSignIn"></div> */}
+        </React.Fragment>
+    ) 
+}
