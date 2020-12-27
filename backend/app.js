a58d5243fb607e8293bc2d51291e5c9398b6e6a1
Charles Wang <wcharles168gmail.com>
2020-12-26 19:54:14 -0500

    added mongodb

--- backend/app.js
+++ backend/app.js
@@ -64,7 +64,22 @@ app.get('/auth/google/callback',
     }
 );
 
-// outsource routing to separate files in route folder
+// MONGODB
+
+const MongoClient = require('mongodb').MongoClient
+const url = 'mongodb://127.0.0.1:27017'
+const dbName = 'scheduleApp' 
+let db 
+
+MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
+    if (err) return console.log(err)
+  
+    // Storing a reference to the database so you can use it later
+    db = client.db(dbName)
+    console.log(`Connected MongoDB: ${url}`)
+    console.log(`Database: ${dbName}`)
+  })
+
 
 app.use(scheduleRouter) // register application level middleware
 // app.use(landingRouter)
